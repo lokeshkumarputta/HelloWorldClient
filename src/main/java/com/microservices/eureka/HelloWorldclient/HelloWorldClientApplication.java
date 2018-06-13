71b79810a0ba38b1acf75bcf924ed2f4f2964662
Lokeshkumar <lokeshkumarputta@gmail.com>
2018-06-13 14:58:28 +0530

    Intital Commit

--- src/main/java/com/microservices/eureka/HelloWorldclient/HelloWorldClientApplication.java
+++ src/main/java/com/microservices/eureka/HelloWorldclient/HelloWorldClientApplication.java
@@ -11,7 +11,6 @@ public class HelloWorldClientApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world client side started ");
-		
 		SpringApplication.run(HelloWorldClientApplication.class, args);
 	}
 }
